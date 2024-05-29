c1bb7a5b7550d939a8838e8586204d2f6df1b193
90duse <duseibnuduse1212@gmail.com>
2024-05-29 16:19:13 +0300

    added an unpdate

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,19 +1,40 @@
+import 'package:basketballteams/logic/logic_code.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
-
-  // get team function
-  Future getTeam() async {
-    var response = await http.get(Uri.https("api.balldontlie.io", "/v1/teams"));
-    // ignore: avoid_print
-    print(response.body);
-  }
-
   @override
   Widget build(BuildContext context) {
     getTeam();
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 232, 242, 249),
+      appBar: AppBar(
+        title: const Text('Basktell Teams'),
+        backgroundColor: const Color.fromARGB(255, 30, 128, 234),
+      ),
+      body: FutureBuilder(
+          future: getTeam(),
+          builder: ((context, snapshot) {
+            // if loading is done
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                itemCount: teams.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(teams[index].teamname),
+                    subtitle: Text(teams[index].city),
+                    leading: Text(teams[index].abbreviation),
+                    trailing: Text(teams[index].division),
+                  );
+                },
+              );
+            }
+
+            //else if still loading
+            else {
+              return Center(child: CircularProgressIndicator());
+            }
+          })),
+    );
   }
 }
